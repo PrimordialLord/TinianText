2f4754f0926ef351371af50826b9e2c22ceab946
Tinian <1736001651@qq.com>
2025-08-20 08:51:12 +0800

    TinianText

---
+++ offline-pyspark/2025-08-19日报/stream-realtime/src/main/java/com/label/domain/DimSkuInfoMsg.java
@@ -0,0 +1,17 @@
+package com.label.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DimSkuInfoMsg implements Serializable {
+    private String skuid;
+    private String spuid;
+    private String c3id;
+    private String tname;
+}
